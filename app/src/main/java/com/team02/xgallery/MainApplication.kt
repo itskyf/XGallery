1108a2076a8ae118ab34e372b2cdb47e92384914
Ky Anh <40734986+itskyf@users.noreply.github.com>
2021-03-13 17:43:52 +0700

    feat: initial commit

---
+++ app/src/main/java/com/team02/xgallery/MainApplication.kt
@@ -0,0 +1,25 @@
+package com.team02.xgallery
+
+import android.app.Application
+import android.content.pm.ApplicationInfo
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class MainApplication : Application(), ImageLoaderFactory {
+    override fun onCreate() {
+        super.onCreate()
+        if (applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE != 0) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(applicationContext)
+            .availableMemoryPercentage(.3)
+            .crossfade(true)
+            .build()
+    }
+}
