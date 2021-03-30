0e63cc0a8653e95009dcaaa8cd7e7c7c1ba1e41c
Sang <enpro1955@gmail.com>
2021-03-30 10:16:47 +0700

    more_button_onclick

--- app/src/main/java/com/team02/xgallery/ui/image/ImageFragment.kt
+++ app/src/main/java/com/team02/xgallery/ui/image/ImageFragment.kt
@@ -2,14 +2,19 @@ package com.team02.xgallery.ui.image
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.team02.xgallery.R
 import com.team02.xgallery.databinding.FragmentImgBinding
 import dagger.hilt.android.AndroidEntryPoint
 
+
 @AndroidEntryPoint
-class ImageFragment : Fragment() {
+class ImageFragment : Fragment(), PopupMenu.OnMenuItemClickListener {
     private var _binding: FragmentImgBinding? = null
     private val binding get() = _binding!!
 
@@ -22,8 +27,35 @@ class ImageFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.moreBtn.setOnClickListener{
+            val popup = PopupMenu(activity, view)
+            popup.inflate(R.menu.menu_img_more)
+            val menu = popup.menu
+            popup.setOnMenuItemClickListener { item -> menuItemClicked(item) }
+        }
+    }
+
+    private fun menuItemClicked(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.more -> Toast.makeText(activity, "Bookmark", Toast.LENGTH_SHORT).show()
+        }
+        return true
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+
+    }
+    override fun onMenuItemClick(item: MenuItem): Boolean {
+        return true
     }
 }
+
+
+
+
+
+
