4eb43516a2f8472b231762f9aa09fc92ba083304
Kieu Cong Hau <kieuconghau2000@gmail.com>
2021-04-01 11:54:48 +0700

    feat: upload photos - pick photos from device

--- app/src/main/java/com/team02/xgallery/MainViewModel.kt
+++ app/src/main/java/com/team02/xgallery/MainViewModel.kt
@@ -1,8 +1,10 @@
 package com.team02.xgallery
 
+import android.net.Uri
 import androidx.lifecycle.ViewModel
 import com.team02.xgallery.data.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 @HiltViewModel
