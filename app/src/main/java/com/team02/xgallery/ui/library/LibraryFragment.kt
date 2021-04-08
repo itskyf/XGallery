61e4202f95e4a06d137fe57fc182905300dcf613
Kieu Cong Hau <kieuconghau2000@gmail.com>
2021-04-08 17:03:42 +0700

    feat: add external storage permission for On Device

--- app/src/main/java/com/team02/xgallery/ui/library/LibraryFragment.kt
+++ app/src/main/java/com/team02/xgallery/ui/library/LibraryFragment.kt
@@ -1,13 +1,16 @@
 package com.team02.xgallery.ui.library
 
+import android.Manifest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.PopupMenu
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import com.team02.xgallery.R
 import com.team02.xgallery.databinding.FragmentLibraryBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,23 +26,47 @@ class LibraryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentLibraryBinding.inflate(inflater, container, false)
-
-        binding.onDeviceButton.setOnClickListener {
-            findNavController().navigate(
-                LibraryFragmentDirections.actionLibraryFragmentToFolderFragment()
-            )
-        }
-
-        binding.sortButton.setOnClickListener {
-            val popup = PopupMenu(requireContext(), it)
-            popup.menuInflater.inflate(R.menu.album_sort, popup.menu)
-            popup.setOnMenuItemClickListener { menuItem: MenuItem ->
-                binding.sortButton.text = menuItem.title
-                true
-            }
-            popup.show()
-        }
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        // ----- External Storage Permission -----
+        val requestPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { granted: Boolean ->
+            if (granted) {
+                findNavController().navigate(
+                    LibraryFragmentDirections.actionLibraryFragmentToFolderFragment()
+                )
+            } else {
+                Snackbar.make(
+                    binding.root,
+                    "Please accept to upload photos.",
+                    Snackbar.LENGTH_SHORT
+                ).setAction("OK") {
+                    // TODO(): Navigate to the Setting Fragment
+                    // navController.navigate(R.id.setting_fragment)
+                }.show()
+            }
+        }
+
+        with(binding) {
+            onDeviceButton.setOnClickListener {
+                requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+
+            sortButton.setOnClickListener {
+                val popup = PopupMenu(requireContext(), it)
+                popup.menuInflater.inflate(R.menu.album_sort, popup.menu)
+                popup.setOnMenuItemClickListener { menuItem: MenuItem ->
+                    binding.sortButton.text = menuItem.title
+                    true
+                }
+                popup.show()
+            }
+        }
+
+        super.onViewCreated(view, savedInstanceState)
     }
 
     override fun onDestroyView() {
