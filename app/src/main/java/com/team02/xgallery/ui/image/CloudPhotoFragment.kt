05bbd2cc3ba24dbb5c4bca6597bc53a8c1c1f7e7
Sang <enpro1955@gmail.com>
2021-04-08 11:22:08 +0700

    Update CloudPhotoFragment.kt

--- app/src/main/java/com/team02/xgallery/ui/image/CloudPhotoFragment.kt
+++ app/src/main/java/com/team02/xgallery/ui/image/CloudPhotoFragment.kt
@@ -6,30 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.team02.xgallery.databinding.FragmentViewPhotoBinding
-import dagger.hilt.android.AndroidEntryPoint
-
-@AndroidEntryPoint
-class CloudPhotoFragment : Fragment() {
-    private var _binding: FragmentViewPhotoBinding? = null
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentViewPhotoBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 import com.team02.xgallery.R
-import com.team02.xgallery.databinding.FragmentCloudPhotoBinding
 import dagger.hilt.android.AndroidEntryPoint
+import com.team02.xgallery.databinding.FragmentCloudPhotoBinding
+
 
 
 @AndroidEntryPoint
