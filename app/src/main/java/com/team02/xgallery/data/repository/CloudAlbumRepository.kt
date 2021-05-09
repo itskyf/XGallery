98e42c3dff0479c9171044b2bf97df8a1dd607dd
Nguyễn Đức Hiếu <51395700+18127099@users.noreply.github.com>
2021-05-09 15:02:43 +0700

    feat: fix thumbnail of album

--- app/src/main/java/com/team02/xgallery/data/repository/CloudAlbumRepository.kt
+++ app/src/main/java/com/team02/xgallery/data/repository/CloudAlbumRepository.kt
@@ -34,7 +34,7 @@ class CloudAlbumRepository {
     fun createAlbum(name: String, listItem: List<Any>) {
         val newCloudAlbum = CloudAlbum(
             name = name,
-            thumbnailId = "pxvXcgeCETcVGBKz1vIl1KMLEqB2/media/0b44d29c-904a-45a0-94ac-b859859dd5f420200722_160350.jpg"
+            thumbnailId = listItem.first().toString()
         )
         val path = "albums/${Date().time}"
         db.document(path).set(newCloudAlbum)
